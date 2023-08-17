2c0cf29cdf8f778604e69bcaba2d2568634eccb4
Nikhil Dhakal <nickarsenal007@gmail.com>
2023-08-17 23:31:23 +0545

    Using store in another component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="about">
         <h1>About</h1>
+        <button @click="storeCounter.increaseCount" class="counter-btn">
+            {{ storeCounter.count }}
+        </button>
     </div>
 </template>
+
+<script setup>
+    import { useCounterStore } from '../stores/counter';
+    const storeCounter = useCounterStore()
+</script>
+
+<style>
+    .counter-btn {
+        font-size: 50px;
+    }
+</style>
