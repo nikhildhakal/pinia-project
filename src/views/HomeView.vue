217f7425b3e680eb1ec9843abfcdf39931bc1864
Nikhil Dhakal <nickarsenal007@gmail.com>
2023-08-17 23:02:05 +0545

    Setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <div class="count">
+            0
+        </div>
+        <div class="buttons">
+            <button>-</button>
+            <button>+</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.count {
+    font-size: 60px;
+    margin: 20px;
+}
+.buttons button {
+    font-size: 40px;
+    margin: 10px;
+    cursor: pointer;
+}
+</style>
